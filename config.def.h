5f238c05c1db72685fed5938f2072fbe32709055
Marc Schlienger <marc.schlienger@posteo.de>
2022-07-03 10:09:33 +0200

    corrects the the resources array

--- config.def.h
+++ config.def.h
@@ -217,7 +217,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
-		{ "alpha",        FLOAT,   &chscale },
+		{ "alpha",        FLOAT,   &alpha },
 };
 
 /*
